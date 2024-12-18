a83dbfb201ccefd774e22bcfdb61b8aa0b670adb
prithwiman97 <prithwiman.mazumdar@gmail.com>
2024-12-19 03:08:40 +0530

    Added formpost redirect to php stub

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Firebase!');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel Node.js app");
+});
+
+app.post("/", (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+  let queryParams = [];
+  for (let key in formData) {
+    queryParams.push(key + "=" + encodeURIComponent(formData[key]));
+  }
+  console.log(queryParams.join("&"));
+  res.redirect(`https://php-stub.free.nf?${queryParams}`);
 });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
